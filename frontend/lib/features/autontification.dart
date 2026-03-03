ac235fdc00f80bb6281a8c369630643cbc63627a
chaimaaberrabah2-cmyk <chaimaaberrabah2@gmail.com>
2026-03-03 17:20:41 +0100

    send

--- frontend/lib/features/autontification.dart
+++ frontend/lib/features/autontification.dart
@@ -8,10 +8,5 @@ class StartingPage extends StatefulWidget {
   State<StartingPage> createState() => _StartingPageState();
 }
 
-class _StartingPageState extends State<StartingPage> {
-  // Defined Color Palette
-  final Color _brightBlue = const Color(0xFF2563EB);
-  
-  
-}
+
 
