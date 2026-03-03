1d23045e5ee6da7a3d1449a01fed8867e22258ac
chaimaaberrabah2-cmyk <chaimaaberrabah2@gmail.com>
2026-03-03 17:22:45 +0100

    add color

--- frontend/lib/features/autontification.dart
+++ frontend/lib/features/autontification.dart
@@ -8,5 +8,14 @@ class StartingPage extends StatefulWidget {
   State<StartingPage> createState() => _StartingPageState();
 }
 
-
+class _StartingPageState extends State<StartingPage> {
+  // Defined Color Palette
+  final Color _brightBlue = const Color(0xFF2563EB);
+  final Color _brightBlue = const Color(0xFF2563EB);
+  final Color _brightBlue = const Color(0xFF2563EB);
+  final Color _brightBlue = const Color(0xFF2563EB);
+  final Color _brightBlue = const Color(0xFF2563EB);
+  
+  
+}
 
